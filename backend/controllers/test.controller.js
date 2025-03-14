7814d5582d53b6851ae6ceb972ca8e40346cd328
Hong Jihyeong <154999028+topograp2@users.noreply.github.com>
2025-03-14 15:07:49 +0900

    chore(#1): Swagger doc 설정

--- backend/controllers/test.controller.js
+++ backend/controllers/test.controller.js
@@ -1,3 +1,33 @@
+/**
+ * @swagger
+ * /api/test/good-day:
+ *   get:
+ *     summary: 테스트 API
+ *     description: 서버 상태를 테스트하고 간단한 메시지를 반환합니다.
+ *     tags:
+ *       - Test
+ *     responses:
+ *       200:
+ *         description: 성공적으로 메시지를 반환합니다.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "좋은 하루 되세요 😁"
+ *       500:
+ *         description: 서버 내부 오류
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Internal Server Error"
+ */
 export const getTest =  async (req, res)=>{
     try{
         res.status(200).json({
