29914b0bc0d4ef3e31a25c99b45f666c95b0892c
Hee Seo Kim <hisounni@ewhain.net>
2025-03-18 09:28:14 +0900

    chore: 로그인 화면 중앙 고정,
    ImageGallery 수정, GlobalStyle 생성

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import NavBar from './components/NavBar';
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
-import { Routes, Route } from "react-router-dom";
+import NavBar from './components/NavBar'; // 네비바 컴포넌트 임포트
 
 const App = () => {
+  const location = useLocation(); // 현재 경로 가져오기
+
   return (
     <div>
-      <NavBar />
+      {/* 로그인 페이지가 아닐 때만 NavBar 표시 */}
+      {location.pathname !== "/login" && <NavBar />}
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
