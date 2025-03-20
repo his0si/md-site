9fbd04abc9076c208b85e0395ea5a70e988b1fa6
laurenjung03 <2376267@ewhain.net>
2025-03-20 17:49:27 +0900

    feat(#19): home,header 부분

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+// import { useState, useEffect } from "react";
+import Header from "./Header";
+// import ProductList from "./ProductList";
+import styled from "styled-components";
 
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  max-width: 500px;
+  background: rgb(248, 250, 243);
+  display: flex;
+  flex-direction: column;
+  // align-items: center;
+  justify-content: center;
+  margin: auto;
+`;
 const Home = () => {
-  return (
-    <div>
-      home
-    </div>
-  )
-}
+  // const [products, setProducts] = useState([]);
+  // // useEffect(()=>{
+  //   const mockData=[
+  // {id:1, name}
+  //
+  // ];
 
-export default Home
+  //   }
+  //   })
+
+  return (
+    <Container>
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+
+      {/* <ProductList products={products} /> */}
+      <h1>just in the process</h1>
+    </Container>
+  );
+};
+
+export default Home;
