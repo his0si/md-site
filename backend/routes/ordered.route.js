ccbd45602bbabe40adc337bf478a6fbba8c122ed
Jade0728 <syeong8663@gmail.com>
2025-03-27 14:04:03 +0900

    fix(#22): 라우터 주소변경

--- backend/routes/ordered.route.js
+++ backend/routes/ordered.route.js
@@ -2,6 +2,6 @@ import express from "express";
 import { createOrdered } from "../controllers/formpost.controller.js";
 const router = express.Router();  // router 선언
 
-router.post("/Ordered",createOrdered)
+router.post("/ordered", createOrdered);
 
 export default router;
