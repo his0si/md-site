4fe2e10f6849d693fe8c22d52793df32aef849db
Jade0728 <syeong8663@gmail.com>
2025-04-01 12:54:18 +0900

    fix(#22): 라우트 오류 수정

--- backend/routes/ordered.route.js
+++ backend/routes/ordered.route.js
@@ -2,6 +2,6 @@ import express from "express";
 import { createOrdered } from "../controllers/formpost.controller.js";
 const router = express.Router();  // router 선언
 
-router.post("/ordered", createOrdered);
+router.post("/", createOrdered);
 
 export default router;
