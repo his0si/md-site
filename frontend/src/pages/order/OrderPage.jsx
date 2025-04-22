7320ad58a92beaa4c37981930751a36617a42ab8
Hong Jihyeong <154999028+topograp2@users.noreply.github.com>
2025-04-22 18:04:12 +0900

    Docs: 상품 수령 시간 추가

--- frontend/src/pages/order/OrderPage.jsx
+++ frontend/src/pages/order/OrderPage.jsx
@@ -334,7 +334,7 @@ const OrderPage = () => {
           <RowWithThickDivider>
             <div>수령 일자/장소</div>
             <div>
-              <strong>25.05.07 ECC 이삼봉홀</strong>
+              <strong>25.05.07 ECC 이삼봉홀 10시~17시</strong>
             </div>
           </RowWithThickDivider>
 
