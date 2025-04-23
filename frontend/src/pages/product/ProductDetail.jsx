c92137115c9a9ebcb84cfd2ad0531a28e1b5ae9c
Hong Jihyeong <154999028+topograp2@users.noreply.github.com>
2025-04-23 13:20:46 +0900

    Docs: 품목 사이즈 안내 추가

--- frontend/src/pages/product/ProductDetail.jsx
+++ frontend/src/pages/product/ProductDetail.jsx
@@ -180,6 +180,9 @@ const ProductDetail = () => {
       <ProductName>{product.productName}</ProductName>
       <ProductPrice>{product.price}</ProductPrice>
       <ProductStatus>
+        {product.productName.includes("M") ? "상세 사이즈 : 가슴단면 50.5 총길이 64" : ""}
+        {product.productName.includes("반다나") ? "상세 사이즈 : 60 x 60" : ""}
+        {product.productName.includes("XL") ? "상세 사이즈 : 가슴단면 55.5 총길이 70" : ""}
         {product.detailImage ? (
           <img
             src={product.detailImage}
